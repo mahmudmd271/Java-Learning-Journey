e5bc7a71ce274d1bbd34d65dcf3a70e39a3785c1
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-02-27 20:28:59 +0600

    Add files via upload

---
+++ BasicOfJava/MultipleVariable.java
@@ -0,0 +1,14 @@
+package BasicOfJava;
+
+public class MultipleVariable {
+    public static void main(String[] args){
+        int x = 5,y = 8,z = 9;
+        System.out.println("The value of x is "+x);
+        System.out.println("The value of y is "+y);        
+        z = 10;        
+        System.out.println("The value of z is "+z);
+        System.out.println(x+y+z);
+        System.out.println(x+y+z+"Total");
+        System.out.println("Hellow pillow"+"world wide");
+    }  
+}
