1ccc2fb9e588541a8cb84e425fb1a6ac1e3c63d4
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-02-27 19:57:18 +0600

    Add files via upload

---
+++ BasicOfJava/BasicsOfJava.java
@@ -0,0 +1,8 @@
+package BasicOfJava;
+
+public class BasicsOfJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ BasicOfJava/variableint.java
@@ -0,0 +1,12 @@
+package BasicOfJava;
+
+public class variableint {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 4;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
