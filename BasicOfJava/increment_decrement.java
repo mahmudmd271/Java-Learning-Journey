a77efa8c042b0f11d054be604fd308349b7e4318
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-02-28 15:48:40 +0600

    Add files via upload

---
+++ BasicOfJava/increment_decrement.java
@@ -0,0 +1,12 @@
+package BasicOfJava;
+
+public class increment_decrement {
+    public static void main(String[] args){
+        int x = 5;        
+        System.out.println(x++);
+        x = 5; 
+        System.out.println(++x);
+        // ++x means pre increament operator.
+        // x++ means post increament operator.
+    }
+}
