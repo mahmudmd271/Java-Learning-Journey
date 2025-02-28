9e42b239880d6ee12ac16057263f1536ee8db5ee
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-02-28 17:04:08 +0600

    Add files via upload

---
+++ BasicOfJava/doubledatatype.java
@@ -0,0 +1,19 @@
+package BasicOfJava;
+
+public class doubledatatype {
+    public static void main(String[] args){
+        double z = 5 / 2;
+        //2.0 instead of 2.5
+        System.out.println(z);        
+        double x = 5;
+        double y = 2;
+        System.out.print("Sum: ");
+        System.out.println(x+y);
+        System.out.print("Difference: ");
+        System.out.println(x-y);
+        System.out.print("Multiplication: ");
+        System.out.println(x*y);
+        System.out.print("Division: ");
+        System.out.println(x/y);
+    }
+}
