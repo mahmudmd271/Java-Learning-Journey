819181e4bda76179bdaded6440ce6bba25e2ce85
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-02-28 15:55:16 +0600

    Add files via upload

---
+++ BasicOfJava/sumof2num.java
@@ -0,0 +1,10 @@
+package BasicOfJava;
+
+public class sumof2num {
+    public static void main(String[] args){
+        int x = 7;
+        int y = 10;
+        System.out.println("Sum of 2 numbers is: "+(x+y));
+        
+    }
+}
