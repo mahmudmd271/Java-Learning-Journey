826d51800e98c7151fa2c69e83c17683acccb158
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-03-27 23:02:04 +0600

    Add files via upload

---
+++ BasicOfJava/AreaOfCircle.java
@@ -0,0 +1,13 @@
+package BasicOfJava;
+
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double r = 8;
+        double pi = 3.1415;
+        double a = pi * r * r;
+        System.out.print("The area of circle: "+a);
+        //System.out.println(a);
+    }
+}
+
