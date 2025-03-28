aec48af92277f5813c8487e926c16cdb035c2c50
Md Mahmud Hossain <158801233+mahmudmd271@users.noreply.github.com>
2025-03-28 21:49:04 +0600

    Create 9. simpleInterest.java

---
+++ BasicOfJava/9. simpleInterest.java
@@ -0,0 +1,16 @@
+package BasicOfJava;
+
+public class simpleInterest {
+    public static void main(String[] args){
+        // Create 4 Box
+        double principle,rate,time,Simple_Interest;
+        principle = 100;
+        rate = 20;
+        time = 3;
+        
+        Simple_Interest = (principle*rate*time)/100;
+        
+        System.out.println(Simple_Interest);
+        
+    }
+}
